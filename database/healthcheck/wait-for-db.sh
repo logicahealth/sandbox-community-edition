d74376838dea36b66e74fa72c92f214b725cefb8
Gopal Menon <gopal@interopion.com>
2021-02-18 18:42:31 -0700

    Added schemas for r5 server to list of schemas to wait on

--- database/healthcheck/wait-for-db.sh
+++ database/healthcheck/wait-for-db.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-target_schema_count="11"
+target_schema_count="13"
 sleep_seconds=2
 
-schema_count=$(mysql -uroot -ppassword -e "show databases;" | grep -E -c 'hspc_8_MasterDstu2Empty|hspc_8_MasterDstu2Smart|hspc_8_MasterR4Empty|hspc_8_MasterR4Smart|hspc_8_MasterStu3Empty|hspc_8_MasterStu3Smart|hspc_8_hspc8|hspc_8_hspc9|hspc_8_hspc10|oic|sandman')
+schema_count=$(mysql -uroot -ppassword -e "show databases;" | grep -E -c 'hspc_8_MasterDstu2Empty|hspc_8_MasterDstu2Smart|hspc_8_MasterR4Empty|hspc_8_MasterR5Empty|hspc_8_MasterR4Smart|hspc_8_MasterStu3Empty|hspc_8_MasterStu3Smart|hspc_8_hspc8|hspc_8_hspc9|hspc_8_hspc10|hspc_8_hspc11|oic|sandman')
 sleep $sleep_seconds
 if [ "$schema_count" -eq $target_schema_count ]
 then
